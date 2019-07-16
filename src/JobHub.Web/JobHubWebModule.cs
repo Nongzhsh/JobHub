f145dc554929839aa3ab058d0f8f82db7335458d
nongzhsh <nongzhsh@outlook.com>
2019-07-16 14:57:33 +0800

    本地化，只启用中文

--- src/JobHub.Web/JobHubWebModule.cs
+++ src/JobHub.Web/JobHubWebModule.cs
@@ -146,10 +146,10 @@ namespace JobHub.Web
                         typeof(AbpUiResource)
                     );
 
-                options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-                options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
+                //options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
+                //options.Languages.Add(new LanguageInfo("en", "en", "English"));
+                //options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+                //options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
                 options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
             });
         }
